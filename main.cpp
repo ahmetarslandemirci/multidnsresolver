6b0b180101173a673c649aa2862a906db69cf014
arslan <dont-send-this-arslan@yopmail.com>
2019-07-07 20:44:30 +0300

    added option parse

--- main.cpp
+++ main.cpp
@@ -11,71 +11,38 @@
 
 int strfind(const char *p, const char c, size_t i = 0);
 void change(const unsigned char *host, unsigned char *h);
+char * resolve(char *url);
 
+void print_usage();
 
-int main() {
-    int sockd;
+int main(int argc, char **argv) {
+    int opt;
+    unsigned char flags = 0;
+    char *url = NULL;
 
-    unsigned char buf[BUF_SIZE];
-
-    struct sockaddr_in dest;
-
-    struct dns_header dns2;
-    query q;
-
-    struct in_addr addr;
-    sockd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-    dest.sin_family = AF_INET;
-    dest.sin_port = htons(53); // dns servers port
-    inet_aton("8.8.8.8", &addr);
-    dest.sin_addr.s_addr = addr.s_addr;
-
-    memset(&dns2, 0, sizeof(struct dns_header));
-    dns2.id = htons( getpid() );
-    dns2.rd = 1;
-    dns2.q_count = htons(1);
-
-    const char *h = "yahoo.com.tr";
-    unsigned char *host = (unsigned char*)malloc(strlen(h)+2);
-    memset(host, 0, strlen(h)+2);
-
-    change( (unsigned char*)h, host );
-    int host_size = strlen((char*)host);
-
-    q.name = host;
-
-    q.ques.qclass = htons(1);
-    q.ques.qtype = htons(1); // dns que type A = 1,
-
-    // create packet
-    memset(buf, 0, BUF_SIZE);
-    memcpy(&buf[0], &dns2, sizeof(struct dns_header));
-    memcpy(&buf[sizeof(struct dns_header)], q.name, host_size+1);
-    memcpy(&buf[sizeof(struct dns_header)+host_size+1 ], &q.ques, sizeof(struct question));
-
-    size_t packet_size = sizeof(struct dns_header)+host_size+1+ sizeof(struct question);
-    if ( sendto(sockd, (char*)&buf, packet_size, 0, (struct sockaddr*)&dest, sizeof(dest)) < 0 ) {
-        std::cout << "sendto error" << std::endl;
+    while((opt = getopt(argc, argv, "u:h")) != -1)
+    {
+        switch(opt)
+        {
+            case 'u':
+                flags |= 0x01;
+                url = optarg;
+                break;
+            case 'h':
+                print_usage();
+                exit(0);
+                break;
+            case ':':
+                printf("option needs a value\n");
+                break;
+            default:
+                exit(0);
+                break;
+        }
     }
-    memset(buf, 0, BUF_SIZE);
-    int i = sizeof(dest), recv_size = 0;
-    if( (recv_size = recvfrom (sockd,(char*)&buf , BUF_SIZE, 0 , (struct sockaddr*)&dest , (socklen_t*)&i )) < 0) {
-        std::cout << "recv error" << std::endl;
-    }
+    char *ip = resolve(url);
+    printf("IP: %s\n", ip);
 
-    in_addr ip;
-    uint32_t p = 0;
-    for (int j = recv_size-4; j < recv_size; ++j) {
-        ip.s_addr <<= 8;
-        ip.s_addr |= (uint8_t)buf[j];
-    }
-    ip.s_addr = htonl(ip.s_addr);
-
-    char *ip_addr = inet_ntoa(ip);
-    std::cout << ip_addr << std::endl;
-
-    free(host);
-    close(sockd);
     return 0;
 }
 
@@ -110,4 +77,80 @@ void change(const unsigned char *host, unsigned char *out) {
         if(curr_pos == -1) { curr_pos = host_size; }
     }
 
+}
+
+void print_usage() {
+    printf("usage: ./multidns -u example.com\n");
+    printf("NOTE: You should write the url without http\n");
+}
+
+char * resolve(char *url) {
+    int sockd;
+
+    unsigned char buf[BUF_SIZE];
+
+    struct sockaddr_in dest;
+
+    struct dns_header dns2;
+    query q;
+
+    struct in_addr addr;
+    sockd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    dest.sin_family = AF_INET;
+    dest.sin_port = htons(53); // dns servers port
+    inet_aton("8.8.8.8", &addr);
+    dest.sin_addr.s_addr = addr.s_addr;
+
+    memset(&dns2, 0, sizeof(struct dns_header));
+    dns2.id = htons( getpid() );
+    dns2.rd = 1;
+    dns2.q_count = htons(1);
+
+    const char *h = url;
+    unsigned char *host = (unsigned char*)malloc(strlen(h)+2);
+    memset(host, 0, strlen(h)+2);
+
+    change( (unsigned char*)h, host );
+    int host_size = strlen((char*)host);
+
+    q.name = host;
+
+    q.ques.qclass = htons(1);
+    q.ques.qtype = htons(1); // dns que type A = 1,
+
+    // create packet
+    memset(buf, 0, BUF_SIZE);
+    memcpy(&buf[0], &dns2, sizeof(struct dns_header));
+    memcpy(&buf[sizeof(struct dns_header)], q.name, host_size+1);
+    memcpy(&buf[sizeof(struct dns_header)+host_size+1 ], &q.ques, sizeof(struct question));
+
+    size_t packet_size = sizeof(struct dns_header)+host_size+1+ sizeof(struct question);
+    if ( sendto(sockd, (char*)&buf, packet_size, 0, (struct sockaddr*)&dest, sizeof(dest)) < 0 ) {
+        std::cout << "sendto error" << std::endl;
+    }
+    memset(buf, 0, BUF_SIZE);
+    int i = sizeof(dest), recv_size = 0;
+    if( (recv_size = recvfrom (sockd,(char*)&buf , BUF_SIZE, 0 , (struct sockaddr*)&dest , (socklen_t*)&i )) < 0) {
+        std::cout << "recv error" << std::endl;
+    }
+    // get ip from last 4 byte
+    in_addr ip;
+    uint32_t p = 0;
+    for (int j = recv_size-4; j < recv_size; ++j) {
+        ip.s_addr <<= 8;
+        ip.s_addr |= (uint8_t)buf[j];
+    }
+    ip.s_addr = htonl(ip.s_addr);
+
+    char *ip_addr = inet_ntoa(ip);
+
+    free(host);
+// todo: parse response
+//    struct dns_header *res_header;
+//    res_header = (struct dns_header *) &buf[0];
+//    int ans_count = ntohs(res_header->ans_count);
+//    printf("%d", ans_count);
+
+    close(sockd);
+    return ip_addr;
 }
